385fefc59780ee157fa659f224fab6c47fe17a15
Suchi <suchibatheja2@gmail.com>
2025-12-16 13:32:07 +0530

    adding Get Request class

---
+++ src/test/java/BasicsGETMethodAutomation.java
@@ -0,0 +1,31 @@
+
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.*;
+
+public class BasicsGETMethodAutomation {
+
+	public static void main(String[] args) {
+		
+		RestAssured.baseURI="https://rahulshettyacademy.com";
+		
+		given().log().all().queryParam("key", "qaclick123")
+		.queryParam("place_id", "a1c536602942a9e36dc2e9323cc62bf6")
+		.header("Content-Type","application/json")
+		
+		.when().post("maps/api/place/get/json")
+		.then().log().all().assertThat().statusCode(200);
+		
+		
+		System.out.println("GET Request Submitted successfully");
+		
+		
+		
+		
+		
+
+		
+		
+	}
+
+}
